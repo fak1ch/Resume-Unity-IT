c830f0f4eabf4824e58aa40db5a44b856915ce71
fak1ch <slyvayva@mail.ru>
2023-05-07 22:01:47 +0300

    AS_main: update animations

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,8 +1,8 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
     var e = document.querySelectorAll(".count");
-    gsap.from(e, { textContent: 0, duration: 0, ease: Power1.easeIn, snap: { textContent: 1 }, stagger: 1 }),
-        gsap.from(".extimation-line", { duration: 0, width: 0, stagger: 1, ease: Power4.easeIn }, 0.5),
+    gsap.from(e, { textContent: 0, duration: 4, ease: Power1.easeIn, snap: { textContent: 1 }, stagger: 1 }),
+        gsap.from(".extimation-line", { duration: 4, width: 0, stagger: 1, ease: Power4.easeIn }, 0.5),
         gsap.fromTo(".about-me", 2, { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, 0.5),
         gsap.fromTo(".header__preview", 2, { x: -200, opacity: 0 }, { x: 0, opacity: 1 }, 0.5),
         gsap.fromTo(".header__preview-circle", 2, { y: -200, opacity: 0 }, { y: 0, opacity: 1 }, 0.5),
